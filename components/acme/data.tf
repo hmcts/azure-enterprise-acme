795ec4fcfa5d0d7db3fcb15bf609e7cf9b145fc6
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-12 09:42:55 +0100

    add data resources

--- components/acme/data.tf
+++ components/acme/data.tf
@@ -4,3 +4,12 @@ data "azurerm_client_config" "current" {
 data "azurerm_subscription" "current" {
   subscription_id = data.azurerm_client_config.current.subscription_id
 }
+
+data "azuread_group" "platform_operations" {
+  display_name     = "DTS Platform Operations"
+  security_enabled = true
+}
+
+data "azuread_group" "dns_contributor" {
+  display_name = "DTS Public DNS Contributor (env:${lower(var.env)})"
+}
