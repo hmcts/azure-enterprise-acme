ceafc7c10654eebcbdf9f95a9c102cbd909d88e6
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-12 08:33:24 +0100

    initial commit

---
+++ components/acme/acme.tf
@@ -0,0 +1,18 @@
+module "acme" {
+
+  source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=DTSPO-9746/acme-kv"
+
+  location                       = var.location
+  env                            = var.env
+  common_tags                    = module.tags.common_tags
+  product                        = var.product
+  subscription_id                = data.azurerm_client_config.current.subscription_id
+  acme_storage_account_repl_type = var.acme_storage_account_repl_type
+}
+
+module "tags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
---
+++ components/acme/variables.tf
@@ -0,0 +1,26 @@
+variable "root_id" {
+  type    = string
+  default = "hmcts"
+}
+
+variable "root_name" {
+  type    = string
+  default = "HMCTS Programmes"
+}
+
+# unused
+variable "env" {}
+variable "builtFrom" {}
+variable "product" {}
+
+variable "subscriptions" {
+  default = {}
+}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "acme_storage_account_repl_type" {
+  default = "ZRS"
+}
