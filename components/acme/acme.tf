90bfb438987ddd5e443782b092d2f8e7f125f7fb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-10-12 09:47:21 +0100

    remove acme module

--- components/acme/acme.tf
+++ components/acme/acme.tf
@@ -1,14 +1,14 @@
-module "acme" {
+# module "acme" {
 
-  source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=DTSPO-9746/acme-kv"
+#   source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=DTSPO-9746/acme-kv"
 
-  location                       = var.location
-  env                            = var.env
-  common_tags                    = module.tags.common_tags
-  product                        = var.product
-  subscription_id                = data.azurerm_client_config.current.subscription_id
-  acme_storage_account_repl_type = var.acme_storage_account_repl_type
-}
+#   location                       = var.location
+#   env                            = var.env
+#   common_tags                    = module.tags.common_tags
+#   product                        = var.product
+#   subscription_id                = data.azurerm_client_config.current.subscription_id
+#   acme_storage_account_repl_type = var.acme_storage_account_repl_type
+# }
 
 module "tags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
