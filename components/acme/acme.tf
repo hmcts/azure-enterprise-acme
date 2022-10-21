77207d9a3dde4000a3719288197fcd6a5081981c
Tyler-34 <tylerm@kainos.com>
2022-10-21 16:21:13 +0100

    fixing module variables

--- components/acme/acme.tf
+++ components/acme/acme.tf
@@ -2,15 +2,15 @@ module "acme" {
 
   source = "git::https://github.com/hmcts/terraform-module-acme-function.git?ref=master"
 
-  location                       = var.location
-  env                            = var.env
-  common_tags                    = module.tags.common_tags
-  product                        = var.product
-  subscription_id                = data.azurerm_client_config.current.subscription_id
-  acme_storage_account_repl_type = var.acme_storage_account_repl_type
-  platform_operations_group      = var.administrator_group_object_id
-  dns_contributor_group          = var.dns_contributor_group_object_id
-  application_id                 = data.azuread_application.appreg.application_id
+  location                         = var.location
+  env                              = var.env
+  common_tags                      = module.tags.common_tags
+  product                          = var.product
+  subscription_id                  = data.azurerm_client_config.current.subscription_id
+  acme_storage_account_repl_type   = var.acme_storage_account_repl_type
+  administrator_group_object_id    = data.azuread_group.platform_operations.object_id
+  dns_contributor_group_object_id  = data.azuread_group.dns_contributor.object_id
+  application_id                   = data.azuread_application.appreg.application_id
 
 }
 
