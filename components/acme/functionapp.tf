3db65205040911b22b3c8a98f7f611ba12146005
Tyler-34 <tylerm@kainos.com>
2022-11-01 09:28:24 +0000

    updating pipeline

--- components/acme/functionapp.tf
+++ components/acme/functionapp.tf
@@ -66,3 +66,8 @@ resource "azurerm_windows_function_app" "funcapp" {
   }
   tags = module.tags.common_tags
 }
+
+resource "azuread_group_member" "dnszonecontributor" {
+  group_object_id  = var.dns_contributor_group_object_id
+  member_object_id = azurerm_windows_function_app.funcapp.identity[0].principal_id
+}
