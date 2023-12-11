8fcbda3bc925e3868d618ab44ae3a1fc5d93896e
Vik <viktoriya.bogdanova@hmcts.net>
2023-12-11 16:40:05 +0000

    add role assignment

--- components/acme/managedidentity.tf
+++ components/acme/managedidentity.tf
@@ -23,7 +23,7 @@ resource "azurerm_role_assignment" "app-proxy-ga-service-connection-secret-manag
 }
 
 resource "azurerm_role_assignment" "app-proxy-ga-service-connection-certificate-management" {
-  count                = var.env == "sandbox" || var.env == "ptl" ? 1 : 0
+  count                = var.env == "sbox" ? 1 : 0
   principal_id         = data.azuread_group.platform_operations.object_id
   role_definition_name = "Key Vault Certificates Officer"
   scope                = azurerm_key_vault.kv.id
