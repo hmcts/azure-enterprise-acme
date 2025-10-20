7cce23899a72260e1ed41c7cc0e40cc09b4b652c
Alex Bance <Alex.Bance@justice.gov.uk>
2025-10-20 10:33:27 +0100

    feat: output package url to allow troubleshooting/confirmation

--- components/acme/local.tf
+++ components/acme/local.tf
@@ -1,6 +1,7 @@
 locals {
-  name                     = replace(lower(data.azurerm_subscription.subscriptionid.display_name), "sharedservices", "sds")
-  app_name                 = "acme-${lower(data.azurerm_subscription.subscriptionid.display_name)}"
-  acme_major_version       = regex("^(v[0-9]+)", var.acme_version)[0]
-  acme_version_no_prefix   = regex("^v(.+)$", var.acme_version)[0]
+  name                   = replace(lower(data.azurerm_subscription.subscriptionid.display_name), "sharedservices", "sds")
+  app_name               = "acme-${lower(data.azurerm_subscription.subscriptionid.display_name)}"
+  acme_major_version     = regex("^(v[0-9]+)", var.acme_version)[0]
+  acme_version_no_prefix = regex("^v(.+)$", var.acme_version)[0]
+  acme_package_url       = "https://stacmebotprod.blob.core.windows.net/keyvault-acmebot/${local.acme_major_version}/${local.acme_version_no_prefix}.zip"
 }
---
+++ components/acme/output.tf
@@ -0,0 +1,4 @@
+output "package_url" {
+  description = "The URL of the ACME bot package."
+  value       = local.acme_package_url
+}
